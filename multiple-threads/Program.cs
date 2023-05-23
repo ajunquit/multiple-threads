a4d1e50ec0e57c7390ac0606292a8e269e7c5a03
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-05-23 00:36:53 -0500

    Add project files.

---
+++ multiple-threads/Program.cs
@@ -0,0 +1,44 @@
+﻿using multiple_threads;
+using System;
+using System.Threading;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Crear instancias de los subprocesos
+        SubProcessA processA = new SubProcessA();
+        SubProcessB processB = new SubProcessB();
+        SubProcessC processC = new SubProcessC();
+        SubProcessD processD = new SubProcessD();
+        SubProcessE processE = new SubProcessE();
+
+        // Crear los hilos para cada subproceso
+        Thread threadA = new Thread(processA.Execute);
+        Thread threadB = new Thread(processB.Execute);
+        Thread threadC = new Thread(processC.Execute);
+        Thread threadD = new Thread(processD.Execute);
+        Thread threadE = new Thread(processE.Execute);
+
+        // Iniciar los hilos independientes A, B, C y E
+        threadA.Start();
+        threadB.Start();
+        threadC.Start();
+        threadE.Start();
+
+        // Esperar a que SubProcessB termine antes de iniciar SubProcessD
+        threadB.Join();
+
+        // Iniciar hilo dependiente D
+        threadD.Start();
+        
+
+        // Esperar a que todos los subprocesos terminen
+        threadA.Join();
+        threadB.Join();
+        threadC.Join();
+        threadD.Join();
+        threadE.Join();
+        Console.WriteLine("Todos los hilos han terminado");
+    }
+}
---
+++ multiple-threads/SubProcess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_threads
+{
+    public class SubProcessA
+    {
+        public void Execute()
+        {
+            Console.WriteLine("SubProcessA running.");
+            Thread.Sleep(1000);
+            Console.WriteLine("SubProcessA executed.");
+        }
+    }
+
+    public class SubProcessB
+    {
+        public void Execute()
+        {
+            Console.WriteLine("SubProcessB running.");
+            Thread.Sleep(10000);
+            Console.WriteLine("SubProcessB executed.");
+        }
+    }
+
+    public class SubProcessC
+    {
+        public void Execute()
+        {
+            Console.WriteLine("SubProcessC running.");
+            Thread.Sleep(1000);
+            Console.WriteLine("SubProcessC executed.");
+        }
+    }
+
+    public class SubProcessD
+    {
+        public void Execute()
+        {
+            Console.WriteLine("SubProcessD running.");
+            Thread.Sleep(7000);
+            Console.WriteLine("SubProcessD executed.");
+        }
+    }
+
+    public class SubProcessE
+    {
+        public void Execute()
+        {
+            Console.WriteLine("SubProcessE running.");
+            Thread.Sleep(1000);
+            Console.WriteLine("SubProcessE executed.");
+        }
+    }
+}
